17d2f3b78c566a783148c84ad26478a403bd9585
Jung kyu Lee <48317269+ljk9410@users.noreply.github.com>
2020-08-05 11:43:59 +0900

    level 1

---
+++ BAEKJOON/level1/calculate.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int     main()
+{
+    int A;
+    int B;
+
+    scanf("%d %d", &A, &B);
+    printf("%d\n", A + B);
+    printf("%d\n", A - B);
+    printf("%d\n", A * B);
+    printf("%d\n", A / B);
+    printf("%d", A % B);
+}
