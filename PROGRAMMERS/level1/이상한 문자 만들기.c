c0c2a6fe0df5cac2e31f29a0532858d725b88d0f
Jung kyu Lee <48317269+ljk9410@users.noreply.github.com>
2020-08-19 13:50:16 +0900

    modified

--- PROGRAMMERS/level1/이상한 문자 만들기.c
+++ PROGRAMMERS/level1/이상한 문자 만들기.c
@@ -8,37 +8,29 @@ char*   solution(const char* s)
     int     i;
     int     j;
 
-    answer = (char*)malloc(sizeof(char) * strlen(s) + 1);
+    answer = (char*)malloc(sizeof(char) * (strlen(s) + 1));
     strcpy(answer, s);
     i = 0;
-    if (answer[i] == " ")
-    {
-        while (answer[i] == " ")
-            i++;
-    }
     while (answer[i] != 0)
     {
         j = 0;
         while ((answer[i + j] != " ") && (answer[i + j] != 0))
         {
-            if (j == 0 || j % 2 == 0)
+            if (j % 2 == 0)
             {
                 if (answer[i + j] >= 'a' && answer[i + j] <= 'z')
                     answer[i + j] -= 32;
             }
-            else if (j % 2 == 1)
+            else
             {
-                if (answer[i + j] <= 'A' && answer[i + j] >= 'Z')
+                if (answer[i + j] >= 'A' && answer[i + j] <= 'Z')
                     answer[i + j] += 32;
             }
             j++;
         }
         i += j;
-        if (answer[i] != 0)
-        {
-            while (answer[i] = " ")
-                i++;
-        }
+        i++;
     }
+    answer[i] = 0;
     return answer;
 }
