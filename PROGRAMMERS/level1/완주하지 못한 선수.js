1aa9de38a2ed6ffe668df4d2fdb38cc4ad8c4f69
Jung kyu Lee <48317269+ljk9410@users.noreply.github.com>
2020-09-20 16:59:43 +0900

    20200920

---
+++ PROGRAMMERS/level1/완주하지 못한 선수.js
@@ -0,0 +1,29 @@
+function solution1(participant, completion) {  
+    const completionObj = completion.reduce((obj, curr) => {
+        obj[curr] = obj[curr] ? (obj[curr] + 1) : 1;
+        return obj;
+    }, {});
+    console.log(completionObj);
+    
+    return participant.find(person => {
+        if (completionObj[person])
+            completionObj[person] -= 1;
+        else
+            return true
+    })
+}
+
+function solution2(participant, completion) {
+    const partSort = participant.sort();
+    const comSort = completion.sort();
+    console.log(partSort, comSort);
+    for(let i in partSort)
+    {
+        if (partSort[i] !== comSort[i])
+            return partSort[i];
+    }
+}
+    
+const participant = ["m", "s", "m", "a"];
+const completion = ["s", "a", "m"];
+console.log(solution2(participant, completion));
